ecf22130c1480614a6c6e5148cc488e30a96812d
Jude Rozario <juderzro08@gmail.com>
2024-05-08 09:42:52 -0400

    First Commit

---
+++ screens/TreasurerScreens/CoachManagement.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { Styles } from "../../Colors.js";
+
+const CoachManagement = () => {
+    return (
+        <View style={Styles.MainContainer}>
+            <Text style={Styles.MainText}>Coach Management</Text>
+        </View>
+    );
+};
+
+export default CoachManagement;
