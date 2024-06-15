57c35c9d0301aa1b2778c579f8b86f7d790f0d1a
Jude Rozario <juderzro08@gmail.com>
2024-06-14 21:18:08 -0400

    First Commit

--- globalFunctions.tsx
+++ globalFunctions.tsx
@@ -1,37 +1,31 @@
 import React from "react";
 import { Eye, EyeOff } from "react-native-feather";
 
-const determineEye = (
-	passwordShow: boolean,
-	color: string,
-): React.JSX.Element => {
-	return passwordShow ? (
-		<Eye color={color} fill={"none"} />
-	) : (
-		<EyeOff color={color} fill={"none"} />
-	);
+const determineEye = (show: boolean): React.JSX.Element => {
+    return show ? (
+        <Eye color={"darkgray"} fill={"none"} />
+    ) : (
+        <EyeOff color={"darkgray"} fill={"none"} />
+    );
 };
 
-class Validity {
-	static validateEmail(text: string): boolean {
-		const emailRegex: RegExp =
-			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return text.toLowerCase().trim().match(emailRegex) ? true : false;
-	}
-	static validateUsername(text: string): boolean {
-		return text.length >= 3;
-	}
-	static validatePassword(text: string): boolean {
-		const passwordRegex: RegExp =
-			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&\.])[A-Za-z\d@$!%*?&\.]{8,}$/;
-		return text.length >= 8 && text.trim().match(passwordRegex) ? true : false;
-	}
-	static validatePhoneNumber(text: string): boolean {
-		const phoneNumberRegex: RegExp =
-			/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
-		return text.trim().match(phoneNumberRegex) ? true : false;
-	}
-}
+const determineEyeWithColor = (
+    valid: boolean,
+    show: boolean,
+): React.JSX.Element => {
+    const col = valid ? "green" : "red";
+    return show ? (
+        <Eye fill={"none"} stroke={col} />
+    ) : (
+        <EyeOff fill={"none"} stroke={col} />
+    );
+};
 
-export { Validity };
-export { determineEye };
+const emailRegex: RegExp =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordRegex: RegExp =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&\.])[A-Za-z\d@$!%*?&\.]{8,}$/;
+const phoneNumberRegex: RegExp =
+    /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+
+export { emailRegex, passwordRegex, phoneNumberRegex, determineEye, determineEyeWithColor };
--- screens/SignUp.tsx
+++ screens/SignUp.tsx
@@ -1,239 +1,241 @@
 import React, { useState } from "react";
 import {
-	Alert,
-	KeyboardAvoidingView,
-	Platform,
-	StyleSheet,
-	Text,
-	TextInput,
-	TouchableOpacity,
-	View,
-	KeyboardTypeOptions,
+    Alert,
+    KeyboardAvoidingView,
+    Platform,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+    KeyboardTypeOptions,
 } from "react-native";
 import { Styles } from "../Colors";
-import { CheckCircle, Eye, EyeOff, X } from "react-native-feather";
+import { CheckCircle, X } from "react-native-feather";
 import { signUpURI } from "../globalRoutes";
 import axios from "axios";
-import { Validity } from "../globalFunctions";
+import {
+    determineEyeWithColor,
+    emailRegex,
+    passwordRegex,
+    phoneNumberRegex,
+} from "../globalFunctions";
 import { TextInputProps } from "react-native-paper";
 
 interface SignUpProps {
-	navigation: any;
+    navigation: any;
 }
 
 const SignUp: React.FC<SignUpProps> = ({ navigation }) => {
-	const [name, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [phoneNumber, setPhoneNumber] = useState("");
-	const [usernameValidity, setUsernameValidity] = useState(false);
-	const [emailValidity, setEmailValidity] = useState(false);
-	const [passwordValidity, setPasswordValidity] = useState(false);
-	const [phoneNumberValidity, setPhoneNumberValidity] = useState(false);
-	const [error, setError] = useState("");
-	const [passwordShow, setPasswordShow] = useState(false);
+    const [name, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [usernameValidity, setUsernameValidity] = useState(false);
+    const [emailValidity, setEmailValidity] = useState(false);
+    const [passwordValidity, setPasswordValidity] = useState(false);
+    const [phoneNumberValidity, setPhoneNumberValidity] = useState(false);
+    const [error, setError] = useState("");
+    const [passwordShow, setPasswordShow] = useState(false);
 
-	const determineEye = (valid: boolean, show: boolean): React.JSX.Element => {
-		const col = valid ? "green" : "red";
-		return show ? (
-			<Eye fill={"none"} stroke={col} />
-		) : (
-			<EyeOff fill={"none"} stroke={col} />
-		);
-	};
+    const handleSignup = (): void => {
+        const userData = {
+            name: name,
+            email: email,
+            password: password,
+            phoneNumber: phoneNumber,
+        };
+        axios
+            .post(`${signUpURI}`, userData)
+            .then((_) => {
+                Alert.alert("User created successfully.");
+                navigation.navigate("Login");
+            })
+            .catch((err) => {
+                if (err.response && err.response.status === 409)
+                    setError("User with that email already exists.");
+                else if (err.response && err.response.status === 400)
+                    setError("Bad request. Please validate the date.");
+                else if (err.response && err.response.status === 500)
+                    setError("Internal server error.");
+                else setError("An error has occured.");
+            });
+    };
 
-	const handleSignup = (): void => {
-		const userData = {
-			name: name,
-			email: email,
-			password: password,
-			phoneNumber: phoneNumber,
-		};
-		axios
-			.post(`${signUpURI}`, userData)
-			.then((_) => {
-				Alert.alert("User created successfully.");
-				navigation.navigate("Login");
-			})
-			.catch((err) => {
-				if (err.response && err.response.status === 409) {
-					setError("User with that email already exists.");
-				} else if (err.response && err.response.status === 400) {
-					setError("Bad request. Please validate the date.");
-				} else if (err.response && err.response.status === 500) {
-					setError("Internal server error.");
-				} else {
-					setError("An error has occured.");
-				}
-			});
-	};
+    const validateEmail = (text: string) => {
+        setEmail(text.replace(" ", ""));
+        setEmailValidity(false);
+        if (text.match(emailRegex)) setEmailValidity(true);
+    };
 
-	const validateEmail = (text: string) => {
-		setEmail(text);
-		setEmailValidity(Validity.validateEmail(email));
-	};
-	const validateUsername = (text: string) => {
-		setUsername(text);
-		setUsernameValidity(Validity.validateUsername(name));
-	};
-	const validatePassword = (text: string) => {
-		setPassword(text.trim());
-		setPasswordValidity(Validity.validatePassword(password));
-	};
-	const validatePhoneNumber = (text: string) => {
-		setPhoneNumber(text.trim());
-		setPhoneNumberValidity(Validity.validatePhoneNumber(phoneNumber));
-	};
-	const inputBoxes = [
-		{
-			placeholder: "Full Name: ",
-			autoComplete: "name" as TextInputProps["autoComplete"],
-			value: name,
-			secureTextEntry: false,
-			keyboardType: "default" as KeyboardTypeOptions,
-			valid: usernameValidity,
-			errorText: "Username must be at least 3 characters long.",
-			onChangeText: (text: string) => validateUsername(text),
-		},
-		{
-			placeholder: "Phone Number: ",
-			autoComplete: "tel" as TextInputProps["autoComplete"],
-			value: phoneNumber,
-			secureTextEntry: false,
-			keyboardType: "phone-pad" as KeyboardTypeOptions,
-			valid: phoneNumberValidity,
-			errorText: "Please enter a valid phone number.",
-			onChangeText: (text: string) => validatePhoneNumber(text),
-		},
-		{
-			placeholder: "Email: ",
-			autoComplete: "email" as TextInputProps["autoComplete"],
-			value: email,
-			secureTextEntry: false,
-			keyboardType: "email-address" as KeyboardTypeOptions,
-			valid: emailValidity,
-			errorText: "Please enter a valid email address.",
-			onChangeText: (text: string) => validateEmail(text),
-		},
-	];
+    const validateUsername = (text: string) => {
+        setUsername(text);
+        setUsernameValidity(false);
+        if (text.length >= 3) setUsernameValidity(true);
+    };
 
-	return (
-		<KeyboardAvoidingView
-			style={Styles.MainContainer}
-			behavior={Platform.OS === "ios" ? "padding" : "height"}
-		>
-			<Text style={[Styles.MainText, styles.headerText]}>Please Sign Up</Text>
-			<View
-				style={{ width: "100%", gap: 24, paddingTop: 20, paddingHorizontal: 8 }}
-			>
-				{inputBoxes.map((inp, idx) => (
-					<View key={idx}>
-						<View style={[Styles.InputBox]}>
-							<Text style={Styles.InputBoxText}>{inp.placeholder}</Text>
-							<TextInput
-								style={Styles.Input}
-								onChangeText={inp.onChangeText}
-								value={inp.value}
-								autoCapitalize="none"
-								autoComplete={
-									inp.autoComplete as TextInputProps["autoComplete"]
-								}
-								keyboardType={inp.keyboardType}
-								secureTextEntry={inp.secureTextEntry}
-							/>
-							{inp.value.length < 1 ? null : inp.valid ? (
-								<CheckCircle
-									fill={"none"}
-									stroke={"green"}
-									style={Styles.Feather}
-								/>
-							) : (
-								<X fill={"none"} stroke={"red"} style={Styles.Feather} />
-							)}
-						</View>
-						<View>
-							{inp.value && !inp.valid ? (
-								<Text style={styles.errorBottomText}>{inp.errorText}</Text>
-							) : null}
-						</View>
-					</View>
-				))}
+    const validatePassword = (text: string) => {
+        setPassword(text.trim());
+        setPasswordValidity(false);
+        if (text.match(passwordRegex)) setPasswordValidity(true);
+    };
 
-				{/*This part is for the passwords*/}
-				<View>
-					<View style={Styles.InputBox}>
-						<Text style={Styles.InputBoxText}>{"Password: "}</Text>
-						<TextInput
-							style={Styles.Input}
-							onChangeText={validatePassword}
-							value={password}
-							autoCapitalize="none"
-							autoComplete={"password"}
-							keyboardType={"default"}
-							secureTextEntry={!passwordShow}
-						/>
-						<TouchableOpacity
-							style={Styles.Feather}
-							onPress={() => setPasswordShow(!passwordShow)}
-						>
-							{password.length < 1
-								? null
-								: determineEye(passwordValidity, passwordShow)}
-						</TouchableOpacity>
-					</View>
-					<View>
-						{password && !passwordValidity ? (
-							<Text style={styles.errorBottomText}>
-								{
-									"Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character."
-								}
-							</Text>
-						) : null}
-					</View>
-				</View>
-			</View>
-			<View style={{ width: "100%", paddingHorizontal: 40, marginBottom: 40 }}>
-				<TouchableOpacity
-					style={Styles.SubmitButton}
-					onPress={() => {
-						if (
-							usernameValidity &&
-							emailValidity &&
-							passwordValidity &&
-							phoneNumberValidity
-						) {
-							handleSignup();
-						}
-					}}
-				>
-					<Text style={Styles.SubmitButtonText}>Sign Up</Text>
-				</TouchableOpacity>
-				{error.length > 1 ? (
-					<Text style={Styles.SubmitButtonErrorText}>{error}</Text>
-				) : null}
-			</View>
-			<View style={Styles.BottomTextContainer}>
-				<Text style={Styles.BottomText}>Already have an account?</Text>
-				<TouchableOpacity
-					onPress={() => {
-						navigation.navigate("Login");
-					}}
-				>
-					<Text style={Styles.BottomTextLink}>Login</Text>
-				</TouchableOpacity>
-			</View>
-		</KeyboardAvoidingView>
-	);
+    const validatePhoneNumber = (text: string) => {
+        setPhoneNumber(text);
+        setPhoneNumberValidity(false);
+        if (text.match(phoneNumberRegex)) setPhoneNumberValidity(true);
+    };
+
+    const inputBoxes = [
+        {
+            placeholder: "Full Name: ",
+            autoComplete: "name" as TextInputProps["autoComplete"],
+            value: name,
+            secureTextEntry: false,
+            keyboardType: "default" as KeyboardTypeOptions,
+            valid: usernameValidity,
+            errorText: "Username must be at least 3 characters long.",
+            onChangeText: (text: string) => validateUsername(text),
+        },
+        {
+            placeholder: "Phone Number: ",
+            autoComplete: "tel" as TextInputProps["autoComplete"],
+            value: phoneNumber,
+            secureTextEntry: false,
+            keyboardType: "phone-pad" as KeyboardTypeOptions,
+            valid: phoneNumberValidity,
+            errorText: "Please enter a valid phone number.",
+            onChangeText: (text: string) => validatePhoneNumber(text),
+        },
+        {
+            placeholder: "Email: ",
+            autoComplete: "email" as TextInputProps["autoComplete"],
+            value: email,
+            secureTextEntry: false,
+            keyboardType: "email-address" as KeyboardTypeOptions,
+            valid: emailValidity,
+            errorText: "Please enter a valid email address.",
+            onChangeText: (text: string) => validateEmail(text),
+        },
+    ];
+
+    return (
+        <KeyboardAvoidingView
+            style={Styles.MainContainer}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <Text style={[Styles.MainText, styles.headerText]}>Please Sign Up</Text>
+            <View
+                style={{ width: "100%", gap: 24, paddingTop: 20, paddingHorizontal: 8 }}
+            >
+                {inputBoxes.map((inp, idx) => (
+                    <View key={idx}>
+                        <View style={[Styles.InputBox]}>
+                            <Text style={Styles.InputBoxText}>{inp.placeholder}</Text>
+                            <TextInput
+                                style={Styles.Input}
+                                onChangeText={inp.onChangeText}
+                                value={inp.value}
+                                autoCapitalize="none"
+                                autoComplete={
+                                    inp.autoComplete as TextInputProps["autoComplete"]
+                                }
+                                keyboardType={inp.keyboardType}
+                                secureTextEntry={inp.secureTextEntry}
+                            />
+                            {inp.value.length < 1 ? null : inp.valid ? (
+                                <CheckCircle
+                                    fill={"none"}
+                                    stroke={"green"}
+                                    style={Styles.Feather}
+                                />
+                            ) : (
+                                <X fill={"none"} stroke={"red"} style={Styles.Feather} />
+                            )}
+                        </View>
+                        <View>
+                            {inp.value && !inp.valid ? (
+                                <Text style={styles.errorBottomText}>{inp.errorText}</Text>
+                            ) : null}
+                        </View>
+                    </View>
+                ))}
+
+                {/*This part is for the passwords*/}
+                <View>
+                    <View style={Styles.InputBox}>
+                        <Text style={Styles.InputBoxText}>{"Password: "}</Text>
+                        <TextInput
+                            style={Styles.Input}
+                            onChangeText={validatePassword}
+                            value={password}
+                            autoCapitalize="none"
+                            autoComplete={"password"}
+                            keyboardType={"default"}
+                            secureTextEntry={!passwordShow}
+                        />
+                        <TouchableOpacity
+                            style={Styles.Feather}
+                            onPress={() => setPasswordShow(!passwordShow)}
+                        >
+                            {password.length < 1
+                                ? null
+                                : determineEyeWithColor(passwordValidity, passwordShow)}
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        {password && !passwordValidity ? (
+                            <Text style={styles.errorBottomText}>
+                                {
+                                    "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+                                }
+                            </Text>
+                        ) : null}
+                    </View>
+                </View>
+            </View>
+            <View style={{ width: "100%", paddingHorizontal: 40, marginBottom: 40 }}>
+                <TouchableOpacity
+                    style={Styles.SubmitButton}
+                    onPress={() => {
+                        if (
+                            usernameValidity &&
+                            emailValidity &&
+                            passwordValidity &&
+                            phoneNumberValidity
+                        ) {
+                            handleSignup();
+                        }
+                    }}
+                >
+                    <Text style={Styles.SubmitButtonText}>Sign Up</Text>
+                </TouchableOpacity>
+                {error.length > 1 ? (
+                    <Text style={Styles.SubmitButtonErrorText}>{error}</Text>
+                ) : null}
+            </View>
+            <View style={Styles.BottomTextContainer}>
+                <Text style={Styles.BottomText}>Already have an account?</Text>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate("Login");
+                    }}
+                >
+                    <Text style={Styles.BottomTextLink}>{" Login"}</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
+    );
 };
 
 export default SignUp;
 const styles = StyleSheet.create({
-	headerText: {
-		marginBottom: 8,
-		marginTop: 64,
-	},
-	errorBottomText: {
-		color: "#EF4444",
-		left: 4,
-	},
+    headerText: {
+        marginBottom: 8,
+        marginTop: 64,
+    },
+    errorBottomText: {
+        color: "#EF4444",
+        left: 4,
+    },
 });
