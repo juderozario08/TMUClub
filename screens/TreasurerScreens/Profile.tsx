6e94abbdea8d48ddfb9e0eea6ff667dfb8526f75
Jude Rozario <juderzro08@gmail.com>
2024-06-21 18:28:29 -0400

    First Commit

--- screens/TreasurerScreens/Profile.tsx
+++ screens/TreasurerScreens/Profile.tsx
@@ -5,43 +5,46 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import { userURI } from "../../globalRoutes";
 
-const Profile = () => {
-	const [userInfo, setUserInfo] = useState({
-		name: "",
-		email: "",
-		phoneNumber: "",
-	});
-	const fetchUserInfo = async () => {
-		try {
-			const id = await AsyncStorage.getItem("id");
-			if (!id) console.error("User ID not found in AsyncStorage.");
-			const response = await axios.get(`${userURI}/${id}`);
-			setUserInfo(response.data);
-		} catch (error: any) {
-			console.error(error.message);
-		}
-	};
+interface ProfileProps {
+    navigation: any;
+}
+const Profile: React.FC<ProfileProps> = ({ navigation }) => {
+    const [userInfo, setUserInfo] = useState({
+        name: "",
+        email: "",
+        phoneNumber: "",
+    });
+    const fetchUserInfo = async () => {
+        try {
+            const id = await AsyncStorage.getItem("id");
+            if (!id) console.error("User ID not found in AsyncStorage.");
+            const response = await axios.get(`${userURI}/${id}`);
+            setUserInfo(response.data);
+        } catch (error: any) {
+            console.error(error.message);
+        }
+    };
 
-	useEffect(() => {
-		fetchUserInfo();
-	}, []);
+    useEffect(() => {
+        fetchUserInfo();
+    }, []);
 
-	return userInfo ? (
-		<View style={Styles.MainContainer}>
-			<Text style={Styles.MainText}>Profile Screen</Text>
-			<View>
-				<Text style={Styles.MainSubText}>Email: {userInfo.email}</Text>
-				<Text style={Styles.MainSubText}>Username: {userInfo.name}</Text>
-				<Text style={Styles.MainSubText}>
-					Phone-Number: {userInfo.phoneNumber}
-				</Text>
-			</View>
-		</View>
-	) : (
-		<View>
-			<Text>Is Loading...</Text>
-		</View>
-	);
+    return userInfo ? (
+        <View style={Styles.MainContainer}>
+            <Text style={Styles.MainText}>Profile Screen</Text>
+            <View>
+                <Text style={Styles.MainSubText}>Email: {userInfo.email}</Text>
+                <Text style={Styles.MainSubText}>Username: {userInfo.name}</Text>
+                <Text style={Styles.MainSubText}>
+                    Phone-Number: {userInfo.phoneNumber}
+                </Text>
+            </View>
+        </View>
+    ) : (
+        <View style={Styles.MainContainer}>
+            <Text style={Styles.WelcomeText}>Is Loading...</Text>
+        </View>
+    );
 };
 
 export default Profile;
